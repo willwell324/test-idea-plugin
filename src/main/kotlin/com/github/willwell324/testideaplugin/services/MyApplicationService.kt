6e49c04ae5b4d555e8379921bce724fb74eeea32
GitHub Action <action@github.com>
2022-10-04 06:15:57 +0000

    Template cleanup

--- src/main/kotlin/com/github/willwell324/testideaplugin/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/willwell324/testideaplugin/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.willwell324.testideaplugin.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.willwell324.testideaplugin.MyBundle
 
 class MyApplicationService {
 
